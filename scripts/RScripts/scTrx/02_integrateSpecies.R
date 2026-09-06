dc8ff7cd78dfb1e2a10692bbe2f37035b22c7d7e
hcnapier <hailey.napier@duke.edu>
2026-09-06 23:28:48 +0000

    set up for first pass species integration

--- scripts/RScripts/scTrx/02_integrateSpecies.R
+++ scripts/RScripts/scTrx/02_integrateSpecies.R
@@ -1,9 +1,20 @@
 # 0.0 Setup ----
 ## 0.1 Load packages ----
-require(dplyr)
-require(Seurat)
+.libPaths(c("~/R/R4.6.0_packages"))
+options(repos = c(CRAN = "https://cloud.r-project.org")) 
+library(SeuratObject)
+library(Seurat)
+library(dplyr)
+library(remotes)
+library(harmony)
+message("----- PACKAGES LOADED -----")
 
-## 0.2 Load data ----
+## 0.2 Load functions ----
+setwd("/hpc/group/vertgenlab/hailey/vertCons/code/vertCons_revision/scripts/RScripts/functions")
+source("normAndCluster.R")
+message("----- FUNCTIONS LOADED -----")
+
+## 0.3 Load data ----
 ### Set up one-to-one orthologs ----
 setwd("/hpc/group/vertgenlab/hailey/vertCons/code/vertCons_revision/data/orthologs")
 ortho1 <- read.delim("human_mouseGuineaPigDogCowGoatMacaque.txt")
@@ -42,6 +53,65 @@ for(currSpecies in speciesnames){
   after <- nrow(orthoAll)
   message(currSpecies, " removed ", before-after, " duplicates")
 }
+message("----- ORTHOLOGS LOADED -----")
+
+### Set up species Seurat objects ----
+setwd("/work/hcn4/260630_vertCons_wd/scTrx/rObjs/")
+speciesnames <- c("human", "mouse", "rat", "rabbit", "guineaPig", "cow", "dog", "macaque", "goat", "pig")
+speciesnames <- sort(speciesnames)
+speciesList <- list()
+for(currSpecies in speciesnames){
+  if(currSpecies == "human"){
+    message("reading human_list.rds")
+    human_list <- readRDS("human_list.rds")
+    human <- CreateSeuratObject(counts = human_list$counts,
+                                meta.data = human_list$metadata)
+    human[["umap"]] <- CreateDimReducObject(embeddings = human_list$umap, key = "UMAP_", assay = "RNA")
+    speciesList[[currSpecies]] <- human
+    message("done")
+  }else{
+    filename <- paste(currSpecies, "rds", sep = ".")
+    message(paste("reading", filename))
+    speciesList[[currSpecies]] <- readRDS(filename)
+    message("done")
+  }
+}
+### Set useful celltype ident ----
+celltype_merge <- c("dog", "goat", "cow", "guineaPig", "macaque", "pig", "rabbit", "human")
+celltypes_merge <- c("mouse")
+celltype <- c("rat")
+cellNames <- c("CTB", "EVT", "STB", "UNC", "BNC", "Stro", "Endo", "Mac", "Epi", "DC", "Invasive tro", "Bcell", "Tcell", "NKcell", "Mono", "GC", "Mes", "Leu", "Neu")
+for(currSpecies in speciesnames){
+  DefaultAssay(speciesList[[currSpecies]]) <- "RNA"
+  if(currSpecies %in% celltype_merge){
+    Idents(speciesList[[currSpecies]]) <- "celltype_merge"
+  }else if(currSpecies %in% celltypes_merge){
+    Idents(speciesList[[currSpecies]]) <- "celltypes_merge"
+  }else if(currSpecies %in% celltype){
+    Idents(speciesList[[currSpecies]]) <- "celltype"
+  }
+  newnames <- gsub("-", "", speciesList[[currSpecies]]@active.ident)
+  matchedIdx <- grepl(tolower("STGC"), tolower(newnames))
+  newnames[matchedIdx] <- "giantCell"
+  for(currCellType in cellNames){
+    matchedIdx <- grepl(tolower(currCellType), tolower(newnames))
+    newnames[matchedIdx] <- currCellType
+  }
+  matchedIdx <- grepl(tolower("SynT"), tolower(newnames))
+  newnames[matchedIdx] <- "STB"
+  matchedIdx <- grepl(tolower("NK"), tolower(newnames))
+  newnames[matchedIdx] <- "NKcells"
+  matchedIdx <- grepl(tolower("SpT"), tolower(newnames))
+  newnames[matchedIdx] <- "SpT"
+  matchedIdx <- grepl(tolower("ST"), tolower(newnames))
+  newnames[matchedIdx] <- "STB"
+  matchedIdx <- grepl(tolower("giantCell"), tolower(newnames))
+  newnames[matchedIdx] <- "s-TGC"
+  
+  speciesList[[currSpecies]]$napierCellTypes <- newnames
+  Idents(speciesList[[currSpecies]]) <- "napierCellTypes"
+}
+message("----- TAN DATASETS LOADED -----")
 
 
 # 1.0 Create one-to-one ortholog Seurat object ----
@@ -68,6 +138,73 @@ for(currSpecies in speciesnames){
   orthoCountMat <- orthoCountMat[keep_idx, ]
   orthoList[[currSpecies]] <- CreateSeuratObject(counts = orthoCountMat, meta.data = metadata)
 }
+message("----- SEURAT OBJECTS FILTERED BY ONE-TO-ONE ORTHOLOGS -----")
+
+# 2.0 Merge Seurat objects ----
+mergeNames <- speciesnames[speciesnames != "human"]
+mergeNames <- c("human", mergeNames)
+speciesObj <- merge(speciesList[["human"]], y = c(orthoList[[1]], 
+                                          orthoList[[2]], 
+                                          orthoList[[3]], 
+                                          orthoList[[4]], 
+                                          orthoList[[5]], 
+                                          orthoList[[6]], 
+                                          orthoList[[7]],
+                                          orthoList[[8]],
+                                          orthoList[[9]]), add.cell.ids = mergeNames)
+speciesObj <- normAndCluster(speciesObj)
+message("----- SEURAT OBJECTS MERGED -----")
 
 
-# 2.0 ----
+# 3.0 Integrate objects ----
+message("Running Harmony integration...")
+speciesObj <- IntegrateLayers(
+  object       = speciesObj,
+  method       = HarmonyIntegration,
+  orig.reduction = "pca",
+  new.reduction  = "integrated_harmony",
+  verbose      = T
+)
+setwd("/work/hcn4/260630_vertCons_wd/scTrx/rObjs/processed")
+saveRDS(speciesObj,"integratedSpecies.rds")
+message("----- HARMONY INTEGRATION DONE -----")
+
+# RPCA Integration
+message("Running RPCA integration...")
+speciesObj <- IntegrateLayers(
+  object       = speciesObj,
+  method       = RPCAIntegration,
+  orig.reduction = "pca",
+  new.reduction  = "integrated.rpca",
+  verbose      = T
+)
+setwd("/work/hcn4/260630_vertCons_wd/scTrx/rObjs/processed")
+saveRDS(speciesObj,"integratedSpecies.rds")
+message("----- RPCA INTEGRATION DONE -----")
+
+# CCA Integration
+message("  Running CCA integration...")
+speciesObj <- IntegrateLayers(
+  object       = speciesObj,
+  method       = CCAIntegration,
+  orig.reduction = "pca",
+  new.reduction  = "integrated.cca",
+  verbose      = T
+)
+setwd("/work/hcn4/260630_vertCons_wd/scTrx/rObjs/processed")
+saveRDS(speciesObj,"integratedSpecies.rds")
+message("----- CCA INTEGRATION DONE -----")
+
+# scVI Integration
+message("  Running scVI integration...")
+speciesObj <- IntegrateLayers(
+  object       = speciesObj,
+  method       = scVIIntegration,
+  orig.reduction = "pca",
+  new.reduction  = "integrated.scvi",
+  verbose      = T
+)
+setwd("/work/hcn4/260630_vertCons_wd/scTrx/rObjs/processed")
+saveRDS(speciesObj,"integratedSpecies.rds")
+message("----- scVI INTEGRATION DONE -----")
+
