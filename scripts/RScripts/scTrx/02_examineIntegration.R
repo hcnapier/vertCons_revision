bfdf38f18b134ff9a75a2563afe49c943c5e5015
hcnapier <hailey.napier@duke.edu>
2026-09-10 22:32:05 +0000

    Examine integration

--- scripts/RScripts/scTrx/02_examineIntegration.R
+++ scripts/RScripts/scTrx/02_examineIntegration.R
@@ -1,5 +1,25 @@
+require(Seurat)
+
 # Load saved data ----
 setwd("/work/hcn4/260630_vertCons_wd/scTrx/rObjs/processed")
-speciesObj <- readRDS("integratedSpecies.rds")
 
-Idents(speciesObj) <- "cluster"
+speciesObj <- readRDS("clustIntSpecies.rds")
+# Harmony
+Idents(speciesObj) <- "clusters_integrated_harmony"
+DimPlot(speciesObj, reduction = "umap_integrated_harmony", label = T)
+
+Idents(speciesObj) <- "napierCellTypes"
+DimPlot(speciesObj, reduction = "umap_integrated_harmony", label = F)
+
+Idents(speciesObj) <- "species"
+DimPlot(speciesObj, reduction = "umap_integrated_harmony", label = F)
+
+# RPCA
+Idents(speciesObj) <- "clusters_integrated_rpca"
+DimPlot(speciesObj, reduction = "umap_integrated_rpca", label = T)
+
+Idents(speciesObj) <- "napierCellTypes"
+DimPlot(speciesObj, reduction = "umap_integrated_rpca", label = F)
+
+Idents(speciesObj) <- "species"
+DimPlot(speciesObj, reduction = "umap_integrated_rpca", label = F)
