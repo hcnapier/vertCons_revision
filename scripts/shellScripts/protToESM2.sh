82ba13cec2eb396fe071618888b7f04cd53069e3
hcnapier <hailey.napier@duke.edu>
2026-09-13 22:02:08 -0400

    bug fixes

--- scripts/shellScripts/protToESM2.sh
+++ scripts/shellScripts/protToESM2.sh
@@ -11,6 +11,8 @@
 #SBATCH --gres=gpu:1
 #SBATCH --partition=gpu-common
 
+export LD_LIBRARY_PATH=/hpc/group/vertgenlab/hailey/software/miniconda3/envs/scrna/lib/gcc/x86_64-conda-linux-gnu/15.2.0:$LD_LIBRARY_PATH
+
 inDir="/work/hcn4/260630_vertCons_wd/scTrx/proteinFastas"
 outDir="/work/hcn4/260630_vertCons_wd/scTrx/esm2Embeddings"
 pythonDir="/hpc/group/vertgenlab/hailey/vertCons/code/vertCons_revision/scripts/pythonScripts"
@@ -19,15 +21,18 @@ pythonDir="/hpc/group/vertgenlab/hailey/vertCons/code/vertCons_revision/scripts/
 fastaList=($(ls ${inDir}/*.fa | sort))
 
 # Select this task's file based on array index
-currInFasta=${fastaList[$SLURM_ARRAY_TASK_ID]}
-speciesName="${currInFasta%%.*}"
+currInFasta=${fastaList[$SLURM_ARRAY_TASK_ID]}i
+filename="${currInFasta##*/}"
+speciesName="${filename%%.*}"
+
+echo ${speciesName}
 
 source  /hpc/group/vertgenlab/hailey/software/miniconda3/etc/profile.d/conda.sh
 conda activate scrna
 
 python ${pythonDir}/proteome_to_esm2_embeddings.py \
         --fasta ${currInFasta} \
-        --species_name ${currSpecies} \
+        --species_name ${speciesName} \
         --out_dir ${outDir}/ \
         --gene_id_regex "gene:(\\S+)"
 
